957c795d92670e8d2f18699a7da04aecaca2a079
Francois Prowse <fp@prowsehouse.com>
2024-07-22 21:10:46 +1000

    Update apply_patch.sh

--- apply_patch.sh
+++ apply_patch.sh
@@ -1,59 +1,24 @@
 #!/bin/bash
 
 #Install needed files 
-apt update
-apt install patch 
+apt-get update && apt-get install -y patch
 
 # Backup the original files that we are patching. 
 mkdir /root/patch-backup
-cp /root/pve-manager/
-
-
-
-# Directories where the patches will be copied
-MANAGER_DIR="/usr/share/pve-manager/js/"
-NETWORK_DIR="/usr/share/perl5/PVE/Network/SDN/"
-ZONE_DIR="/usr/share/perl5/PVE/Network/SDN/"
-COMMON_DIR="/usr/share/perl5/PVE/"
-
-# Patch file names in the current directory
-MANAGER_PATCH_FILE="manager.patch"
-NETWORK_PATCH_FILE="network.patch"
-ZONE_PATCH_FILE="zones.patch"
-COMMON_PATCH_FILE="common.patch"
+cp /usr/share/pve-manager/js/pvemanagerlib.js /root/patch-backup/
+cp /usr/share/perl5/PVE/Network/SDN/VnetPlugin.pm /root/patch-backup/
+cp /usr/share/perl5/PVE/Network/SDN/Zones.pm /root/patch-backup/
+cp /usr/share/perl5/PVE/Network.pm /root/patch-backup/
 
 # Copy the patch files to the respective directories
-curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/common.patch -o ${COMMON_DIR}/${COMMON_PATCH_FILE}
-curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/network.patch -o ${NETWORK_DIR}/${NETWORK_PATCH_FILE}
-curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/zones.patch -o ${ZONE_DIR}/${ZONE_PATCH_FILE}
-curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/manager.patch -o ${MANAGER_DIR}/${MANAGER_PATCH_FILE}
+curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/manager.patch -o /var/tmp/manager.patch
+curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/network.patch -o /var/tmp/network.patch
+curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/zones.patch -o /var/tmp/zones.patch
+curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/common.patch -o /var/tmp/common.patch
 
-
-# Function to prompt for confirmation
-prompt_to_patch() {
-    local PATCH_DIR=$1
-    local PATCH_FILE=$2
-    local PATCH_NAME=$3
-
-    read -p "Do you want to apply the $PATCH_NAME patch? (y/n): " choice
-    case "$choice" in
-        y|Y )
-            echo "Applying $PATCH_NAME patch..."
-            (cd $PATCH_DIR && patch -p1 < $PATCH_FILE)
-            if [ $? -eq 0 ]; then
-                echo "$PATCH_NAME patch applied successfully."
-            else
-                echo "Failed to apply $PATCH_NAME patch."
-            fi
-            ;;
-        n|N ) echo "Skipping $PATCH_NAME patch.";;
-        * ) echo "Invalid choice. Skipping $PATCH_NAME patch.";;
-    esac
-}
-
-# Prompt the user and apply the patches if confirmed
-prompt_to_patch $MANAGER_DIR "manager.patch" "manager"
-prompt_to_patch $NETWORK_DIR "network.patch" "network"
-prompt_to_patch $COMMON_DIR "common.patch" "common"
+patch -p1 /usr/share/pve-manager/js/pvemanagerlib.js /var/tmp/manager.patch
+patch -p1 /usr/share/perl5/PVE/Network/SDN/VnetPlugin.pm /var/tmp/network.patch
+patch -p1 /usr/share/perl5/PVE/Network/SDN/Zones.pm /var/tmp/zones.patch
+patch -p1 /usr/share/perl5/PVE/Network.pm /var/tmp/common.pm
 
 echo "Script execution completed."
