1ed289acf04b8a470442bc72df46621002306a7c
Francois Prowse <fp@prowsehouse.com>
2024-07-22 21:15:22 +1000

    Update apply_patch.sh

--- apply_patch.sh
+++ apply_patch.sh
@@ -19,7 +19,7 @@ curl -k https://raw.githubusercontent.com/farsonic/proxmox_isolation_patch/main/
 #Patch each file
 patch -p1 /usr/share/pve-manager/js/pvemanagerlib.js /var/tmp/manager.patch
 patch -p1 /usr/share/perl5/PVE/Network/SDN/VnetPlugin.pm /var/tmp/network.patch
-patch -p1 /usr/share/perl5/PVE/Network/SDN/Zones.pm /var/tmp/zones.patch
+patch -p1 /usr/share/perl5/PVE/Network/SDN/Zones/Plugin.pm /var/tmp/zones.patch
 patch -p1 /usr/share/perl5/PVE/Network.pm /var/tmp/common.patch
 
 echo "Script execution completed."
